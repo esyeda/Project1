7448a3536561cbed316e56f3daef3b199d56b204
Eshaal Syeda <eshaal.syeda@richmond.edu>
2025-07-01 01:53:57 +0000

    Add working GenAI feature

---
+++ Project1-PairProgramming.py
@@ -0,0 +1,31 @@
+''' Codio Day5 module: Google GenAi API. Used that lesson to write the following code'''
+import os 
+from google import genai
+from google.genai import types
+
+# Set environment variables
+my_api_key = os.getenv('GENAI_KEY')
+genai.api_key = my_api_key
+
+# Create an genAI client using the key from our environment variable
+client = genai.Client(
+    api_key=my_api_key,
+)
+
+# User Inputs
+city_input = input("Enter the city that youre looking suggestions for: ")
+category_input = input("What are you looking for — a hotel, restaurant, or attraction? ")
+
+# AI Prompt
+prompt = f'recommend a toprated {category_input} in {city_input} and explain why its the better choice.'
+
+# Specify the model to use and the messages to send
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    config=types.GenerateContentConfig(
+      system_instruction="You are a friendly travel assistant who gives helpful suggestions."
+    ),
+    contents=prompt,
+)
+
+print(response.text)
