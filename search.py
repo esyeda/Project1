b51a4d45e1f60abc03d78873d72524d5e55e2d42
wiidu <wiiidu315@gmail.com>
2025-06-30 21:07:39 -0500

    Can fetch API keys now

--- search.py
+++ search.py
@@ -1,4 +1,11 @@
 import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+trip_api_key = os.getenv('TRIP_API_KEY')
+genai_key = os.getenv('GENAI_KEY')
 
 url = "https://api.content.tripadvisor.com/api/v1/location/search?language=en"
 headers = {"accept": "application/json"}
