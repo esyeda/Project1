88ec3c259a9de6e9538beed62d7404ee6344cb1a
wiidu <wiiidu315@gmail.com>
2025-07-02 12:12:07 -0500

    Replanning

--- json_parsing.py
+++ json_parsing.py
@@ -31,41 +31,45 @@ class Parser:
                     GROUP BY Name
                 );"""
             connection.execute(db.text(remove_dupes))
-            # query_result =
-            # connection.execute
-            # (db.text(f"SELECT * FROM {tb_name};")).fetchall()
-            # print(pd.DataFrame(query_result))
-        self.get_ratings()
+        # self.get_ratings()
 
-    def get_ratings(self):
-        if not isinstance(self.json, type([])):
-            return
-        rows = []
-        for location in self.json:
-            location_id = int(location['location_id'])
-            url = (f"https://api.content.tripadvisor.com/api/v1/location/"
-            f"{location_id}/details")
-            headers = {"accept": "application/json"}
-            data = {
-                'key': self.key
-            }
+    # def get_ratings(self):
+    #     if not isinstance(self.json, type([])):
+    #         return
+    #     rows = []
+    #     for location in self.json:
+    #         location_id = int(location['location_id'])
+    #         url = (f"https://api.content.tripadvisor.com/api/v1/location/"
+    #         f"{location_id}/details")
+    #         headers = {"accept": "application/json"}
+    #         data = {
+    #             'key': self.key
+    #         }
 
-            r = requests.get(url, headers=headers, params=data)
-            if r.status_code == 200:
-                flattened = pd.json_normalize(r.json())
-                print(flattened)
-                rows.append(flattened)
-        df = pd.concat(rows, ignore_index=True)
-        df = df.infer_objects()
-        print(df)
-        print(df.dtypes)
-        # df.to_sql("temp", con=self.engine, if_exists='append', index=False)
+    #         r = requests.get(url, headers=headers, params=data)
+    #         if r.status_code == 200:
+    #             flattened = pd.json_normalize(r.json())
+    #             rows.append(flattened)
+    #     df = pd.concat(rows, ignore_index=True)
+    #     for col in df.columns:
+    #         if df[col].apply(lambda x: isinstance(x, (dict, list))).any():
+    #             df[col] = df[col].apply(json.dumps)
+    #     df = df.infer_objects()
+    #     print(df)
+    #     print(df.dtypes)
+    #     # df.to_sql("temp", con=self.engine, if_exists='append', index=False)
 
-        join_command = """CREATE TABLE recommendations AS
-         SELECT * FROM locations
-         JOIN temp ON locations.location_id = temp.location_id;"""
+    #     join_command = """CREATE TABLE recommendations AS
+    #      SELECT * FROM locations
+    #      JOIN temp ON locations.location_id = temp.location_id;"""
+    #     with self.engine.connect() as connection:
+    #         connection.execute(db.text(join_command))
+    #     self.drop("temp")
+        
+    def drop(self, table_name):
+        command = f"DROP TABLE IF EXISTS {table_name}"
         with self.engine.connect() as connection:
-            connection.execute(db.text(join_command))
+            connection.execute(db.text(command))
 
 with open('sampleJSON.txt', 'r') as file:
     location = file.read()
