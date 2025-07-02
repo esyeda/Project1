9f60c4db6dda010fe5fe165d0b96828469e7c01e
wiidu <wiiidu315@gmail.com>
2025-07-02 18:14:12 -0500

    Trying to fix yaml

--- combined.py
+++ combined.py
@@ -29,8 +29,20 @@ data = {
 response = requests.get(url, params=data)
 data_of_trip = response.json()
 
+# results = data_of_trip.get('data', [])
+
+'''top_five = []
+
+for given in results[:5]:
+    name = given.get('name')
+    if name:
+        top_five.append(name)
+
+
+combined = ','.join(top_five)'''
+
 parser = Parser(data_of_trip)
-parser.write_to_database("cate")
+parser.write_to_database(cate)
 
 # Pull stored results
 db_results = parser.pull_list(cate, cy)
@@ -56,7 +68,7 @@ for row in db_results:
 
     print(ai_response.text)
 
-    sugg = input("Want another suggestion? (Yes/No): ").lower().strip()
-    if sugg != "yes":
+    another_sugg = input("Do you want another suggestion? (Yes/No): ").lower().strip()
+    if another_sugg != "yes":
         print("Enjoy!")
         break
